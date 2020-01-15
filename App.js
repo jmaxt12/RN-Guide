ab032df6bf63d960c55c38f260ddf99c19454a9c
Jake Thomas <jmaxt12@gmail.com>
2020-01-14 18:06:30 -0700

    changed styled from inline to to use styleSheet

--- App.js
+++ App.js
@@ -3,11 +3,11 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{padding: 50}}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
         <TextInput 
           placeholder='Course Goal'
-          style={{ width: '80%', borderColor: 'black', borderWidth: 1, padding: 10 }}/>
+          style={styles.textInput}/>
         <Button title='ADD'/>
       </View>
       <View>
@@ -18,5 +18,18 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    alignItems: 'center'
+  },
+  textInput: {
+    width: '80%', 
+    borderColor: 'black', 
+    borderWidth: 1, 
+    padding: 10
+  }
 });
