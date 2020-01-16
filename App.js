4ffeee6eb28eaf722ee77efb4ec777bbc8bf12d0
Jake Thomas <jmaxt12@gmail.com>
2020-01-16 16:50:11 -0700

    able to add items to goal llist

--- App.js
+++ App.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+  }
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
         <TextInput 
           placeholder='Course Goal'
-          style={styles.textInput}/>
-        <Button title='ADD'/>
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}/>
+        <Button 
+          title='ADD' 
+          onPress={addGoalHandler}/>
       </View>
       <View>
-
+        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
       </View>
     </View>
   );
